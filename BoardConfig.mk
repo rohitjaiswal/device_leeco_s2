8bb75ed0ecd3c16d3cd1c4a3949e0702dfede36a
Rohit Jaiswal <jaiswal.rohit6@gmail.com>
2017-05-06 07:29:29 +0530

    s2: Enable dexpreopt for userdebug builds

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -189,10 +189,10 @@ BOARD_HARDWARE_CLASS += device/leeco/s2/cmhw
 
 # Enable dexpreopt to speed boot time
 ifeq ($(HOST_OS),linux)
-  ifeq ($(call match-word-in-list,$(TARGET_BUILD_VARIANT),user),true)
+  ifneq ($(TARGET_BUILD_VARIANT),eng)
     ifeq ($(WITH_DEXPREOPT),)
       WITH_DEXPREOPT := true
-    endif
+   endif
   endif
 endif
 
