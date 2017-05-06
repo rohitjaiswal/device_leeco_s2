0424680d9170d4c91641604e70ea9f61add72797
Rohit Jaiswal <jaiswal.rohit6@gmail.com>
2017-05-07 03:14:49 +0530

    Prepared for DU

---
+++ AndroidProducts.mk
@@ -0,0 +1,2 @@
+PRODUCT_MAKEFILES := \
+     $(LOCAL_DIR)/du_s2.mk
--- device.mk
+++ device.mk
@@ -142,7 +142,7 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    Snap
+    camera.msm8952
 
 # Charger
 PRODUCT_PACKAGES += \
