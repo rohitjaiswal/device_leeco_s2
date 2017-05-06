81893876de311e5e07f4663da87d4f686cc25630
Rohit Jaiswal <jaiswal.rohit6@gmail.com>
2017-05-06 08:52:22 +0530

    Revert "s2: Nuke CMHW"
    This reverts commit ea92f58994d694dbcf057c7e804727b08c216e1d.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -180,6 +180,8 @@ WIFI_DRIVER_FW_PATH_STA := "sta"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 TARGET_USES_WCNSS_MAC_ADDR_REV := true
 
+# Cyanogen hardware
+BOARD_HARDWARE_CLASS += device/leeco/s2/cmhw
 
 # Enable dexpreopt to speed boot time
 ifeq ($(HOST_OS),linux)
