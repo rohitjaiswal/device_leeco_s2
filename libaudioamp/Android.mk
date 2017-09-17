0340ce291a754b3799b2082eb9f2f72b5d773a1f
Rohit Jaiswal <jaiswal.rohit6@gmail.com>
2017-09-17 13:36:49 +0000

    s2: fixing libaudioamp
    Signed-off-by: Rohit Jaiswal <jaiswal.rohit6@gmail.com>

--- libaudioamp/Android.mk
+++ libaudioamp/Android.mk
@@ -17,6 +17,9 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_C_INCLUDES += \
+    $(TOP)/hardware/qcom/audio/hal/ \
+    $(TOP)/hardware/qcom/audio-caf/hal/ \
+    $(TOP)/hardware/qcom/audio-caf-los/msm8952/hal/ \
     $(call project-path-for,qcom-audio)/hal/ \
     $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include \
     external/tinyalsa/include \
